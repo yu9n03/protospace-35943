fc5293528c9a5bf419662f42075617a1a17ad7a8
yu9n03 <yu9n03@gmail.com>
2021-06-18 15:31:58 +0900

    編集後のリダイレクト先の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -34,7 +34,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to root_path 
+      redirect_to action: :show
     else
       render :edit
     end
